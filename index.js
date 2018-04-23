038575a8c3e11cd97a4bb08c144ce3d50c5e56f9
Marjory Marquez <samaterra@hotmail.com>
2018-04-22 21:29:03 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -9,49 +9,30 @@ function myFunction() {
         x.className = "topnav";
     }
 } 
-
 //section data background//
 $(document).ready(function(){
     $('section[data-type="background"]').each(function(){
-        var $bgobj = $(this); // assigning the object
-    
+        var $bgobj = $(this); // assigning the object  
         $(window).scroll(function() {
             var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-            
+          
 // Put together our final background position
-            var coords = '50% '+ yPos + 'px';
+      var coords = '50% '+ yPos + 'px';
 
 // Move the background
-            $bgobj.css({ backgroundPosition: coords });
+        $bgobj.css({ backgroundPosition: coords });
         }); 
     });    
 });
 
 //End Section Background//
-
 //Section About//
-
 //End Section About//
-
 //Section Skills//
-
-
 //End Section Skills//
-
-
 //Section Projects//
-
 //End Section Projects//
-
-
 //Section Contact//
-
-
 //End Section Contact//
-
-
-
 //Section Footer//
-
-
 //End Section Footer//
