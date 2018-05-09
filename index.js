9ab6909747369967f7e1733a8797d0ebc96d2b48
Marjory Marquez <samaterra@hotmail.com>
2018-05-08 23:06:30 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ $(document).ready(function(){
         var $bgobj = $(this); // assigning the object  
         $(window).scroll(function() {
             var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-          
-// Put together our final background position
+ 
+// Put final background position
       var coords = '50% '+ yPos + 'px';
 
 // Move the background
@@ -24,11 +24,41 @@ $(document).ready(function(){
         }); 
     });    
 });
+$(document).ready(function($) {
+
+	$('.card__share > a').on('click', function(e){ 
+		e.preventDefault() // prevent default action - hash doesn't appear in url
+   		$(this).parent().find( 'div' ).toggleClass( 'card__social--active' );
+		$(this).toggleClass('share-expanded');
+    });
+  
+});
 
 //End Section Background//
 //Section About//
 //End Section About//
 //Section Skills//
+
+//animation//
+var $window = $(window);
+var $elem = $(".animation")
+
+function isScrolledIntoView($elem, $window) {
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+$(document).on("scroll", function () {
+    if (isScrolledIntoView($elem, $window)) {
+        $elem.addClass("animation")
+    }
+});
+//END animation//
+
 //End Section Skills//
 //Section Projects//
 //End Section Projects//
