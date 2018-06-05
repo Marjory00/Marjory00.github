2c2b76e72e175d0471d06fc1c412e950b66acdf7
Marjory Marquez <samaterra@hotmail.com>
2018-06-05 01:33:36 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-//Section TopNav//
+
+//Section topnav//
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-        x.className += "responsive";
+        x.className += " responsive";
     } else {
         x.className = "topnav";
     }
 } 
-//END Section TopNav//
-
-//Section Home Background//
 //section data background//
 $(document).ready(function(){
     $('section[data-type="background"]').each(function(){
@@ -32,69 +30,62 @@ $(document).ready(function($) {
 		e.preventDefault() // prevent default action - hash doesn't appear in url
    		$(this).parent().find( 'div' ).toggleClass( 'card__social--active' );
 		$(this).toggleClass('share-expanded');
-    }); 
+    });
+  
 });
 
 //End Section Background//
-//END Section Home Background//
-//ANIMATION COL//
+//Section About//
+//End Section About//
+//Section Skills//
 
-//END ANIMATION COL//
-//Top Button//
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+//animation//
+var $window = $(window);
+var $elem = $(".animation")
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
+function isScrolledIntoView($elem, $window) {
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+$(document).on("scroll", function () {
+    if (isScrolledIntoView($elem, $window)) {
+        $elem.addClass("animation")
+    }
+});
+//END animation//
+
+//Create a Controllable Grid View Using JavaScript//
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a "loop" variable
+var i;
+
+// Full-width images
+function one() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "100%";
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
-//END TOP Button//
+// Two images side by side
+function two() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "50%";
+    }
+}
 
-//Title Home Test Animation//
-document.getElementById("test").children[0].className += " load";
-//END Title Test Animation//
+// Four images side by side
+function four() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "25%";
+    }
+}
 
-//Animation PC Section//
-$(document).ready(function(){
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-  
-  $(window).scroll(function() {
-    $(".slideanim").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slide");
-        }
-    });
-  });
-})
+//END Create a Controllable Grid View Using JavaScript//
